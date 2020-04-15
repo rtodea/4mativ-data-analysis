ae248a685973bcd0b9beb1ec34b59051d6a92038
rtodea <robert.todea@e-spres-oh.com>
2020-04-15 13:09:03 +0300

    1 file modified

--- one-step-gps/samples.bash
+++ one-step-gps/samples.bash
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 
 export TOKEN=kWHzHnN10Zy4z2z93cu0EgVm5bSsSFhw5NfYMHCgmzo
+export OSG_API=https://track.onestepgps.com/v3/api/public
 
 curl -H "Authorization: Bearer $TOKEN" \
-	https://track.onestepgps.com/v3/api/public/device \
+	"$OSG_API/device" \
 	| jq .
 
 curl -H "Authorization: Bearer $TOKEN" \
-	https://track.onestepgps.com/v3/api/public/user/me \
+	"$OSG_API/user/me" \
 	| jq .
 
 curl -H "Authorization: Bearer $TOKEN" \
-	"https://track.onestepgps.com/v3/api/public/device-point?device_id=6cTYEBoaQq1KbV81f07-0-" \
+	"$OSG_API/device-point?device_id=6cTYEBoaQq1KbV81f07-0-" \
 	| jq .
